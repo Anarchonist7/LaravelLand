f0eb742a478faa3c6111d75193c98e8536e3ed07
Dawson Ley <Anarchonist@protonmail.com>
2019-05-21 00:38:19 -0700

    updated styles to make page creation easier

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,32 +1,33 @@
-@extends('layout');
+@extends('layout')
 
 @section ('content')
 
-    <h1 class='ganon'>Create a page</h1>
+    <h1 class='page-text-container'>Create a page</h1>
+    <div class="page-text-container">
+        <form method="POST" action="/pages">
+            {{ csrf_field() }}
+            <label for='title'>Title:</label>
+            <input id='title' type='text' name='title'/>
+            <br>
+            <br>
+            <label for='body'>Content</label>
+            <textarea id='body' name='body'></textarea>
+            <br>
+            <br>
+            <br>
+            <button type='submit'>Confirm Creation</button>
 
-    <form method="POST" action="/pages">
-        {{ csrf_field() }}
-        <label for='title'>Title:</label>
-        <input id='title' type='text' name='title'/>
-<br>
-<br>
-        <label for='body'>Content</label>
-        <textarea id='body' name='body'></textarea>
-<br>
-<br>
-<br>
-        <button type='submit'>Confirm Creation</button>
+            @if (count($errors))
+                <div class='ganon'>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-        @if (count($errors))
-            <div class='ganon'>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-    </form>
+        </form>
+    </div>
 
 @endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,9 +2,9 @@
 
 @section ('content')
     <div class="page-text-container">
-        <p class="ganon">dorfssss</p>
-        <br>
         <a class="ganon" href="/pages/show">Here beee yeee pages</a>
+        <br>
+        <br>
         <br>
         <a class="ganon" href="/pages/create">Create a page, if yee dare</a>
     </div>
